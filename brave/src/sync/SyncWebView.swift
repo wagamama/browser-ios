c032ab84449e574452032ccbf5b1c38a0bf9f8eb
Garvan Keeley <garvankeeley@gmail.com>
2017-01-13 10:42:37 -0500

    Move the sync config to JS injected from native

--- brave/src/sync/SyncWebView.swift
+++ brave/src/sync/SyncWebView.swift
@@ -15,13 +15,25 @@ import WebKit
  http.send(params);
  */
 
-class SyncWebView: UIViewController, WKScriptMessageHandler {
+class SyncWebView: UIViewController {
     var webView: WKWebView!
 
     var webConfig:WKWebViewConfiguration {
         get {
             let webCfg = WKWebViewConfiguration()
             let userController = WKUserContentController()
+
+            var config = "var injected_seed = new Uint8Array([243, 203, 185, 143, 101, 184, 134, 109, 69, 166, 218, 58, 63, 155, 158, 17, 31, 184, 175, 52, 73, 80, 190, 47, 45, 12, 59, 64, 130, 13, 146, 248]); " +
+                        "var injected_deviceId = new Uint8Array([1,2,3,4]); "
+            #if DEBUG
+                config += "const injected_braveSyncConfig = {apiVersion: '0', serverUrl: 'https://sync-staging.brave.com', debug:true}"
+            #else
+                config += "const injected_braveSyncConfig = {apiVersion: '0', serverUrl: 'https://sync.brave.com'}"
+            #endif
+
+            userController.addUserScript(WKUserScript(source: config, injectionTime: .AtDocumentEnd, forMainFrameOnly: true))
+
+            userController.addScriptMessageHandler(self, name: "syncToIOS")
 
             ["fetch", "ios-sync", "bundle"].forEach() {
                 userController.addUserScript(WKUserScript(source: getScript($0), injectionTime: .AtDocumentEnd, forMainFrameOnly: true))
@@ -39,16 +51,15 @@ class SyncWebView: UIViewController, WKScriptMessageHandler {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.frame = CGRectMake(20, 20, 100, 100)
+        view.frame = CGRectMake(20, 20, 300, 300)
         webView = WKWebView(frame: view.frame, configuration: webConfig)
-        webView.navigationDelegate = self
+        //webView.navigationDelegate = self
         view.addSubview(webView)
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        //webView.loadHTMLString("<body>TEST<body>", baseURL: NSURL(string: "http://localhost:8000")!)
-        webView.loadRequest(NSURLRequest(URL: NSURL(string: "http://localhost:8000")!))
+        webView.loadHTMLString("<body>TEST</body>", baseURL: nil)
     }
 
     func webView(webView: WKWebView, didFinish navigation: WKNavigation!) {
@@ -56,9 +67,12 @@ class SyncWebView: UIViewController, WKScriptMessageHandler {
     }
 }
 
-extension SyncWebView: WKNavigationDelegate {
+extension SyncWebView: WKScriptMessageHandler {
     func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
-
+        print("😎 \(message.body)")
+        guard let data = message.body as? [String: AnyObject] else { assert(false) ; return }
+        print(data)
     }
+
 }
 
--- brave/src/sync/ios-sync.js
+++ brave/src/sync/ios-sync.js
@@ -2,7 +2,7 @@
 console.log = (function (old_function) {
     return function (text) {
         old_function(text);
-        document.write(text);
+        document.write("<div style='font-size:25'>"+ text +"</div>");
     };
 } (console.log.bind(console)));
 
@@ -10,32 +10,25 @@ console.log = (function (old_function) {
 
 if (!self.chrome || !self.chrome.ipc) {
     var initCb = () => {}
-    // Saved crypto seed; byteArray
-    var seed = new Uint8Array([243, 203, 185, 143, 101, 184, 134, 109, 69, 166, 218, 58, 63, 155, 158, 17, 31, 184, 175, 52, 73, 80, 190, 47, 45, 12, 59, 64, 130, 13, 146, 248])
-    // Saved deviceId; byteArray
-    var deviceId = null
-    // Fill this in using ex: server/config/default.json
-    const config = {
-        apiVersion: '0',
-        serverUrl: 'https://sync.brave.com'
-    }
 
     self.chrome = {}
     const ipc = {}
     ipc.on = (message, cb) => {
+        window.webkit.messageHandlers.syncToIOS.postMessage({message, cb});
         if (message === 'got-init-data') {
             if (cb) {
                 initCb = cb
             }
-            initCb(null, seed, deviceId, config)
+            initCb(null, injected_seed, injected_deviceId, injected_braveSyncConfig)
         }
     }
     ipc.send = (message, arg1, arg2) => {
-        if (message === 'save-init-data') {
-            seed = arg1
-            deviceId = arg2
-            ipc.on('got-init-data')
-        }
+        window.webkit.messageHandlers.syncToIOS.postMessage({message, arg1, arg2});
+//        if (message === 'save-init-data') {
+//            seed = arg1
+//            deviceId = arg2
+//            ipc.on('got-init-data')
+//        }
     }
     self.chrome.ipc = ipc
 
