80b694a485a996f1bc0222977a4f18a02985934e
Garvan Keeley <garvankeeley@gmail.com>
2017-01-31 12:08:20 -0500

    messaging is working

--- brave/src/sync/ios-sync.js
+++ brave/src/sync/ios-sync.js
@@ -1,4 +1,3 @@
-
 console.log = (function (old_function) {
     return function (text) {
         old_function(text);
@@ -6,24 +5,24 @@ console.log = (function (old_function) {
     };
 } (console.log.bind(console)));
 
-
-
 if (!self.chrome || !self.chrome.ipc) {
     var initCb = () => {}
 
     self.chrome = {}
     const ipc = {}
     ipc.on = (message, cb) => {
-        window.webkit.messageHandlers.syncToIOS.postMessage({message, cb});
+        window.webkit.messageHandlers.syncToIOS_on.postMessage({on_msg: message});
         if (message === 'got-init-data') {
             if (cb) {
                 initCb = cb
             }
-            initCb(null, injected_seed, injected_deviceId, injected_braveSyncConfig)
+            var injected_deviceId = new Uint8Array([1,2,3,4]);
+            var injected_braveSyncConfig = {apiVersion: '0', serverUrl: 'https://sync-staging.brave.com', debug:true}
+            initCb(null, null, injected_deviceId, injected_braveSyncConfig)
         }
     }
     ipc.send = (message, arg1, arg2) => {
-        window.webkit.messageHandlers.syncToIOS.postMessage({message, arg1, arg2});
+        window.webkit.messageHandlers.syncToIOS_send.postMessage({message: message, arg1: arg1, arg2: arg2});
 //        if (message === 'save-init-data') {
 //            seed = arg1
 //            deviceId = arg2
