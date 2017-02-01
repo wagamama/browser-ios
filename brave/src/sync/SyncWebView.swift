b723e59e0d2279e31e6deffc73989ba0256735f9
Garvan Keeley <garvankeeley@gmail.com>
2017-02-01 12:32:44 -0500

    Working on getting a sync fetch to happen.Current error: Unsupported sync category: 0.

--- brave/src/sync/SyncWebView.swift
+++ brave/src/sync/SyncWebView.swift
@@ -2,17 +2,17 @@ import UIKit
 import WebKit
 
 /*
- var http = new XMLHttpRequest();
- var url = "https://sync-staging.brave.com/abcdefg/credentials";
- var params = "";
- http.open("POST", url, true);
- http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
- http.onreadystatechange = function() {
-     if(http.readyState == 4 && http.status == 200) {
-         alert(http.responseText);
-     }
+ module.exports.categories = {
+ BOOKMARKS: '0',
+ HISTORY_SITES: '1',
+ PREFERENCES: '2'
  }
- http.send(params);
+
+ module.exports.actions = {
+ CREATE: 0,
+ UPDATE: 1,
+ DELETE: 2
+ }
  */
 
 class SyncWebView: UIViewController {
@@ -23,14 +23,26 @@ class SyncWebView: UIViewController {
             let webCfg = WKWebViewConfiguration()
             let userController = WKUserContentController()
 
-//            var config = "var injected_deviceId = new Uint8Array([1,2,3,4]); "
-//            #if DEBUG
-//                config += "const injected_braveSyncConfig = {apiVersion: '0', serverUrl: 'https://sync-staging.brave.com', debug:true}"
-//            #else
-//                config += "const injected_braveSyncConfig = {apiVersion: '0', serverUrl: 'https://sync.brave.com'}"
-//            #endif
-//
-//            userController.addUserScript(WKUserScript(source: config, injectionTime: .AtDocumentEnd, forMainFrameOnly: true))
+            guard let deviceId = getDeviceIdAsJSArray() else {
+                print("SyncLog: failed to get device id")
+                return webCfg
+            }
+            var config = "var injected_deviceId = new Uint8Array(\(deviceId)); "
+
+            if let seed = getSeedAsJSArray() {
+                config += "var injected_seed = new Uint8Array(\(seed)); "
+            } else {
+                config += "var injected_seed = null; "
+            }
+
+            #if DEBUG
+                config += "const injected_braveSyncConfig = {apiVersion: '0', serverUrl: 'https://sync-staging.brave.com', debug:true}"
+            #else
+                config += "const injected_braveSyncConfig = {apiVersion: '0', serverUrl: 'https://sync.brave.com'}"
+            #endif
+
+            userController.addUserScript(WKUserScript(source: config, injectionTime: .AtDocumentEnd, forMainFrameOnly: true))
+
 
             userController.addScriptMessageHandler(self, name: "syncToIOS_on")
             userController.addScriptMessageHandler(self, name: "syncToIOS_send")
@@ -65,13 +77,70 @@ class SyncWebView: UIViewController {
     func webView(webView: WKWebView, didFinish navigation: WKNavigation!) {
         print(#function)
     }
+
+    // return "[1,2,3,4,...,16]" for sending to javascript
+    func getDeviceIdAsJSArray() -> String? {
+        let prefName = "device-id-js-array"
+        var key = NSUserDefaults.standardUserDefaults().stringForKey(prefName)
+        if key == nil || key!.isEmpty {
+            var uuidBytes = [UInt8](count: 16, repeatedValue: 0)
+            guard let identifier = UIDevice.currentDevice().identifierForVendor else { return nil }
+            identifier.getUUIDBytes(&uuidBytes)
+            let data = NSData(bytes: &uuidBytes, length: 16)
+            let dataAsArray = Array(UnsafeBufferPointer(start: UnsafePointer<UInt8>(data.bytes), count: data.length))
+            key = "\(dataAsArray)" // conveniently formats as [1,2,3,4...]
+            NSUserDefaults.standardUserDefaults().setValue(key, forKey: prefName)
+        }
+        return key
+    }
+
+    func saveSeed(seed: String) {
+        // SwiftKeychainWrapper
+        print("TODO: save seed in keychain")
+    }
+
+    func getSeedAsJSArray() -> String? {
+        // todo get from keychain
+        return "[62, 109, 38, 113, 48, 33, 85, 86, 26, 37, 230, 142, 34, 79, 26, 231, 16, 126, 204, 5, 202, 59, 108, 187, 95, 211, 108, 181, 21, 188, 127, 148]"
+    }
 }
 
 extension SyncWebView: WKScriptMessageHandler {
     func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
-        print("😎 \(message.name) \(message.body)")
-        guard let data = message.body as? [String: AnyObject] else { assert(false) ; return }
-        print(data)
+        //print("😎 \(message.name) \(message.body)")
+        guard let data = message.body as? [String: AnyObject], let messageName = data["message"] as? String else {
+            assert(false) ;
+            return
+        }
+
+        switch messageName {
+        case "save-init-data" :
+            if let seed = data["arg1"] as? String { // strange format "{ 0 = 23; 1 = 204; 10 = 151; }"
+                print(seed)
+                saveSeed(seed)
+            } else {
+                print("Seed expected.")
+            }
+        case "sync-debug":
+            print("Sync Debug: \(data)")
+        case "sync-ready":
+            break
+        case "fetch-sync-records":
+            /*  browser -> webview: sent to fetch sync records after a given start time from the sync server.
+            @param Array.<string> categoryNames, @param {number} startAt (in seconds) **/
+            webView.evaluateJavaScript("callbackList['fetch-sync-records'](null, ['0','1','2'], 0)",
+                                       completionHandler: { (result, error) in
+                print(result)
+                print(error)
+            })
+            break
+        case "send-sync-records":
+            /* browser -> webview, sends this to the webview with the data that needs to be synced to the sync server.
+            @param {string} categoryName, @param {Array.<Object>} records */
+            break
+        default:
+            print("\(messageName) not handled yet")
+        }
     }
 
 }
--- brave/src/sync/ios-sync.js
+++ brave/src/sync/ios-sync.js
@@ -5,34 +5,36 @@ console.log = (function (old_function) {
     };
 } (console.log.bind(console)));
 
+var callbackList = {} // message name to callback function
+
 if (!self.chrome || !self.chrome.ipc) {
     var initCb = () => {}
 
     self.chrome = {}
     const ipc = {}
     ipc.on = (message, cb) => {
-        window.webkit.messageHandlers.syncToIOS_on.postMessage({on_msg: message});
+        window.webkit.messageHandlers.syncToIOS_on.postMessage({message: message});
         if (message === 'got-init-data') {
             if (cb) {
                 initCb = cb
             }
-            var injected_deviceId = new Uint8Array([1,2,3,4]);
-            var injected_braveSyncConfig = {apiVersion: '0', serverUrl: 'https://sync-staging.brave.com', debug:true}
-            initCb(null, null, injected_deviceId, injected_braveSyncConfig)
+            // native has injected these varibles into the js context, or from 'save-init-data'
+            initCb(null, injected_seed, injected_deviceId, injected_braveSyncConfig)
+        } else {
+            callbackList[message] = cb
         }
     }
     ipc.send = (message, arg1, arg2) => {
         window.webkit.messageHandlers.syncToIOS_send.postMessage({message: message, arg1: arg1, arg2: arg2});
-//        if (message === 'save-init-data') {
-//            seed = arg1
-//            deviceId = arg2
-//            ipc.on('got-init-data')
-//        }
+        if (message === 'save-init-data') {
+            injected_seed = arg1
+            injected_deviceId = arg2
+            ipc.on('got-init-data')
+        }
     }
     self.chrome.ipc = ipc
 
     chrome.ipcRenderer = chrome.ipc
 }
-
 
 
