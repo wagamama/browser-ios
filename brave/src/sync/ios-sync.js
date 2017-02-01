413bc4391265052850947d58bdeb9657aa08982c
Garvan Keeley <garvankeeley@gmail.com>
2017-02-01 15:13:14 -0500

    Sync staging branch init is working, yay

--- brave/src/sync/ios-sync.js
+++ brave/src/sync/ios-sync.js
@@ -8,33 +8,18 @@ console.log = (function (old_function) {
 var callbackList = {} // message name to callback function
 
 if (!self.chrome || !self.chrome.ipc) {
-    var initCb = () => {}
-
     self.chrome = {}
     const ipc = {}
+
     ipc.on = (message, cb) => {
+        callbackList[message] = cb
         window.webkit.messageHandlers.syncToIOS_on.postMessage({message: message});
-        if (message === 'got-init-data') {
-            if (cb) {
-                initCb = cb
-            }
-            // native has injected these varibles into the js context, or from 'save-init-data'
-            initCb(null, injected_seed, injected_deviceId, injected_braveSyncConfig)
-        } else {
-            callbackList[message] = cb
-        }
     }
+
     ipc.send = (message, arg1, arg2) => {
         window.webkit.messageHandlers.syncToIOS_send.postMessage({message: message, arg1: arg1, arg2: arg2});
-        if (message === 'save-init-data') {
-            injected_seed = arg1
-            injected_deviceId = arg2
-            ipc.on('got-init-data')
-        }
     }
-    self.chrome.ipc = ipc
 
+    self.chrome.ipc = ipc
     chrome.ipcRenderer = chrome.ipc
 }
-
-
