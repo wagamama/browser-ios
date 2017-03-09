a5691e89b92d4c009840a65680b039fdccafc2ee
Joel Reis <joelhreis@gmail.com>
2017-03-09 10:34:02 -0800

    Updated nice ware imports.

--- brave/tests_src/unit/NicewareTest.swift
+++ brave/tests_src/unit/NicewareTest.swift
@@ -1,6 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import Foundation
 import XCTest
+@testable import Client
+import Shared
 
 class NicewareTest: XCTestCase {
     
@@ -29,7 +32,7 @@ class NicewareTest: XCTestCase {
                 return
             }
             
-            XCTAssertEqual(input, expectedOut)
+            XCTAssertEqual(resultWithStrings, expectedOut)
             
             expect.fulfill()
         }
