94ec2cda70cdb6ad9f92e42e73243a9aa497553e
Joel Reis <joelhreis@gmail.com>
2017-03-22 10:05:52 -0400

    Updated sync js file to properly handle Uint8Arrays.

--- brave/src/sync/ios-sync.js
+++ brave/src/sync/ios-sync.js
@@ -10,16 +10,22 @@ var callbackList = {} // message name to callback function
 if (!self.chrome || !self.chrome.ipc) {
     self.chrome = {}
     const ipc = {}
+    const stringifyArrayCleaner = (k,v) => {
+        if (v instanceof Uint8Array) {
+            return Array.from(v)
+        }
+        return v
+    }
 
     ipc.once = (message, cb) => {
         callbackList[message] = cb
-        window.webkit.messageHandlers.syncToIOS_on.postMessage(JSON.stringify({message: message}));
+        window.webkit.messageHandlers.syncToIOS_on.postMessage(JSON.stringify({message: message}, stringifyArrayCleaner));
     }
     
     ipc.on = ipc.once
 
     ipc.send = (message, arg1, arg2) => {
-        window.webkit.messageHandlers.syncToIOS_send.postMessage(JSON.stringify({message: message, arg1: arg1, arg2: arg2}));
+        window.webkit.messageHandlers.syncToIOS_send.postMessage(JSON.stringify({message: message, arg1: arg1, arg2: arg2}, stringifyArrayCleaner));
     }
 
     self.chrome.ipc = ipc
