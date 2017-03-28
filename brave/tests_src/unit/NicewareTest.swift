7d0cb2940df012d32777800581cc9e3cb57921d7
Joel Reis <joelhreis@gmail.com>
2017-03-28 09:47:51 -0400

    Added nice ware test.

--- brave/tests_src/unit/NicewareTest.swift
+++ brave/tests_src/unit/NicewareTest.swift
@@ -7,6 +7,15 @@ import Shared
 
 class NicewareTest: XCTestCase {
     
+    func testSplitBytes() {
+        let split = Niceware().splitBytes(fromJoinedBytes: "00ee4a423aa3a30f595fc200fa6ad9c96338bb020c375b9298e7687984bae19f")
+        XCTAssertNotNil(split)
+        
+        guard let split2 = split else { return }
+        
+        XCTAssertEqual(split2, [0x00, 0xee, 0x4a, 0x42, 0x3a, 0xa3, 0xa3, 0x0f, 0x59, 0x5f, 0xc2, 0x00, 0xfa, 0x6a, 0xd9, 0xc9, 0x63, 0x38, 0xbb, 0x02, 0x0c, 0x37, 0x5b, 0x92, 0x98, 0xe7, 0x68, 0x79, 0x84, 0xba, 0xe1, 0x9f])
+    }
+    
     func testNewByteSeed() {
         let niceware = Niceware()
         let byteCount = 8
